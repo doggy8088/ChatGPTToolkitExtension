162f11ba3786fca424c791e04dda0b58c4787735
Will <doggy.huang@gmail.com>
2024-10-01 00:42:33 +0800

    更新 CUSTOM_PROMPTS.js 範本

--- CUSTOM_PROMPTS.js
+++ CUSTOM_PROMPTS.js
@@ -3,22 +3,22 @@ customPrompts.push({
     "enabled": true,
     "title": "提供反思",
     "altText": "",
-    "prompt": "Your task is to carefully read a source text and a translation from English to Traditional Chinese, and then give constructive criticism and helpful suggestions to improve the translation. The final style and tone of the translation should match the style of Traditional Chinese colloquially spoken in Taiwan. When writing suggestions, pay attention to whether there are ways to improve the translation's \n(i) accuracy (by correcting errors of addition, mistranslation, omission, or untranslated text),\n(ii) fluency (by applying Traditional Chinese grammar, spelling and punctuation rules, and ensuring there are no unnecessary repetitions),\n(iii) style (by ensuring the translations reflect the style of the source text and takes into account any cultural context),\n(iv) terminology (by ensuring terminology use is consistent and reflects the source text domain; and by only ensuring you use equivalent idioms Traditional Chinese).\nWrite a list of specific, helpful and constructive suggestions for improving the translation. Each suggestion should address one specific part of the translation. Output only the suggestions and nothing else.",
+    "prompt": "請反思上面的翻譯結果，指出其中不符合中文表達習慣、不通順、不夠信雅達的地方",
     "autoSubmit": true
 });
 customPrompts.push({
     "enabled": true,
-    "title": "優化回應",
+    "title": "重新意譯",
     "altText": "",
-    "prompt": "Please take into account the expert suggestions when editing the translation. Edit the translation by ensuring:\n\n(i) accuracy (by correcting errors of addition, mistranslation, omission, or untranslated text),\n(ii) fluency (by applying {target_lang} grammar, spelling and punctuation rules and ensuring there are no unnecessary repetitions),\n(iii) style (by ensuring the translations reflect the style of the source text)\n(iv) terminology (inappropriate for context, inconsistent use), or\n(v) other errors.\n\nOutput only the new translation and nothing else.",
+    "prompt": "請基於上面的反思結果重新意譯",
     "autoSubmit": true
 });
 
 customPrompts.push({
-    "enabled": false,
+    "enabled": true,
     "title": "提升品質",
     "altText": "有時候 ChatGPT 會回答出錯誤、不合邏輯的答案，透過重新審視答案，可以大幅提昇回應品質，提高正確率。",
-    "prompt": "請再審視一次你的回答，你確定你寫的是正確的嗎？",
+    "prompt": "請再審視一次你的回答，你確定你寫的是正確的嗎？請在深思後重新回答我。",
     "autoSubmit": true
 });
 customPrompts.push({
